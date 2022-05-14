71087f29037fafa532b4591808efb19e905f81ac
Angelo <angelomaximodd@outlook.com>
2022-05-14 19:04:04 -0300

    Storage working. But need fix the math between available days and use. It cannot be -1 when we use.

--- rest-days/test.js
+++ rest-days/test.js
@@ -1,19 +1,27 @@
 const hud = document.getElementById('hud'); 
 const add = document.getElementById('add');
 const use = document.getElementById('use');
+const avail = document.getElementById('avail');
 
 let days = 0;
 
+
+// Store and display day number in the HUD.
+let storeDay = () => { 
+    localStorage.setItem("available days", days);
+    // Pick and display the available days object, which is the variable days
+    hud.innerText = localStorage.getItem("available days");
+}
+
 add.onclick = () => {
     days++;
-    hud.innerText = days;
+    storeDay();
 }
 
 use.onclick = () => {
     days = days - 1;
-    hud.innerText = days;
-    localStorage.setItem("available days", days);
+    storeDay();
 }
 
-
-
+// Display available days. Also, avail means available.
+avail.innerText = localStorage.getItem("available days");
